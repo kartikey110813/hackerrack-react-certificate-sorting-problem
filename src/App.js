e27c46a2be0131297b249e273ee6680bb65b9078
kartikey110813@gmail.com <kartikey110813@gmail.com>
2021-06-24 01:41:03 +0530

    code added

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React,{useState} from 'react';
+import './App.css';
+import 'h8k-components';
+
+import Articles from './components/Articles';
+
+const title = "Sorting Articles";
+
+function App({articles}) {
+    const sortedByUpvotes = articles.sort((a,b) => b.upvotes - a.upvotes)
+    const [updatedArticles, setUpdatedArticles] = useState(sortedByUpvotes);
+
+    return (
+        <div className="App">
+            <h8k-navbar header={title}></h8k-navbar>
+            <div className="layout-row align-items-center justify-content-center my-20 navigation">
+                <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
+                <button data-testid="most-upvoted-link" onClick={() => setUpdatedArticles(articles.slice().sort((a,b) => b.upvotes - a.upvotes))} className="small">Most Upvoted</button>
+                <button data-testid="most-recent-link"  onClick={() => setUpdatedArticles(articles.slice().sort((a,b) => new Date(b.date)- new Date(a.date)))} className="small">Most Recent</button>
+            </div>
+            <Articles articles={updatedArticles}/>
+        </div>
+    );
+
+}
+
+export default App;
